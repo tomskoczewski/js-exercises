b9bcd0e3594d57919ce3898fefa2f0db01400ff5
Tomasz Skoczewski <tskoczeks@gmail.com>
2020-05-22 20:15:56 +0200

    slot machine

---
+++ exercise16/Wallet.js
@@ -0,0 +1,30 @@
+class Wallet {
+    constructor(money) {
+        let _money = money;
+
+        this.getWalletValue = () => _money;
+
+        this.checkCanPlay = value => {
+            if (_money >= value) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        this.changeWallet = (value, type) => {
+            if (typeof value === "number" && !isNaN(value)) {
+                if (type === "+") {
+                    return _money += value;
+                } else if (type === "-") {
+                    return _money -= value;
+                } else {
+                    throw new Error("Invalid action type");
+                }
+            } else {
+                throw new Error("Invalid number");
+            }
+        }
+    }
+
+}
